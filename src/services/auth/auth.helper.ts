9f6c1e80b991c08f064cb2db9ec360e191ad168e
KVP <vasiakuruts@gmail.com>
2023-11-08 23:07:47 +0200

    Create auth service

---
+++ src/services/auth/auth.helper.ts
@@ -0,0 +1,27 @@
+import { IAuthResponse, ITokens } from '@/store/user/user.interface'
+import Cookies from 'js-cookie'
+
+export const getAccessToken = async () => {
+	const accessToken = Cookies.get('accessToken')
+	return accessToken || null
+}
+
+export const getUserFromStorage = async () => {
+	return JSON.parse(localStorage.getItem('user') || '{}')
+}
+
+export const saveTokensStorage = (data: ITokens) => {
+	Cookies.set('accessToken', data.accessToken)
+	Cookies.set('refreshToken', data.refreshToken)
+}
+
+export const removeFromStorage = () => {
+	Cookies.remove('accessToken')
+	Cookies.remove('refreshToken')
+	localStorage.removeItem('user')
+}
+
+export const saveToStorage = (data: IAuthResponse) => {
+	saveTokensStorage(data)
+	localStorage.setItem('user', JSON.stringify(data.user))
+}
