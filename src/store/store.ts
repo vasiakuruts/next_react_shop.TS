26827411ad7ac075f1cb77734616dca92d178cde
KVP <vasiakuruts@gmail.com>
2023-11-12 23:24:22 +0200

    create user actions and slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -13,6 +13,7 @@ import storage from 'redux-persist/lib/storage'
 
 import { carouselSlice } from './carousel/carousel.slice'
 import { cartSlice } from './cart/cart.slice'
+import { userSlice } from './user/usrer.slice'
 
 const persistConfig = {
 	key: 'Shop',
@@ -22,7 +23,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
 	cart: cartSlice.reducer,
-	carousel: carouselSlice.reducer
+	carousel: carouselSlice.reducer,
+	user: userSlice.reducer
 })
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
