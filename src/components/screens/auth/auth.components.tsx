94917e5f90e6209d9baeb3a23fd69bb02643db99
KVP <vasiakuruts@gmail.com>
2023-11-20 17:27:25 +0200

    Create button, heading

--- src/components/screens/auth/auth.components.tsx
+++ src/components/screens/auth/auth.components.tsx
@@ -1,8 +1,23 @@
+import Button from '@/components/ui/button/button'
+import Heading from '@/components/ui/heading/heading'
 import Meta from '@/components/ui/meta/meta'
-import { FC } from 'react'
+import { useActions } from '@/hooks/useActions'
+import { useAuth } from '@/hooks/useAuth'
+import { FC, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 const AuthComponents: FC = () => {
-  return <Meta title='Auth'>Auth</Meta>
+  const { isLoading } = useAuth()
+  const { login, register } = useActions()
+
+  const [type, setType] = useState<'login' | 'register'>('login')
+  const {} = useForm()
+  return (
+    <Meta title='Auth'>
+      <Heading>Auth</Heading>
+      <Button variant='orange'>Auth</Button>
+    </Meta>
+  )
 }
 
 export default AuthComponents
