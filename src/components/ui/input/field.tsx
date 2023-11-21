e0d99182bb05f9ae5a02748b46b34510f5aa8a7e
KVP <vasiakuruts@gmail.com>
2023-11-21 19:28:13 +0200

    Fix auth component
    -Create valid email file
    -Create field and interface

---
+++ src/components/ui/input/field.tsx
@@ -0,0 +1,36 @@
+import cn from 'clsx'
+import { forwardRef } from 'react'
+import { IField } from './field.interface'
+
+const Field = forwardRef<HTMLInputElement, IField>(
+  (
+    { placeholder, error, className, type = 'text', style, Icon, ...rest },
+    ref
+  ) => {
+    return (
+      <div className={cn('mb-4 ', className)} style={style}>
+        <label>
+          <span className='mb-1 block'>
+            {Icon && <Icon className='mr-3' />}
+            {placeholder}
+          </span>
+          <input
+            ref={ref}
+            type={type}
+            placeholder={placeholder}
+            {...rest}
+            className={cn(
+              'px-4 py-2 w-full outline-none border border-gray border-solid focus:border-primary transition-all rounded-lg shadow-sm ',
+              {
+                'border-red': !!error
+              }
+            )}
+          />
+        </label>
+        {error && <div className='text-red mt-1 text-sm'>{error}</div>}
+      </div>
+    )
+  }
+)
+Field.displayName = 'Field'
+export default Field
